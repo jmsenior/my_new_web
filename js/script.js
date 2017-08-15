99fbf6445be301d40fe641040f21306b327e66db
José Manuel Serna Rodríguez <jm.sernarodriguez@gmail.com>
2017-08-15 18:13:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function portfolio(){
 
     document.querySelector('#block-menu').innerHTML = "<div id='portfolio'>\
     <ul class='menu-list'>\
-    <li><a class='menu-links' href='#'>Productos</a></li>\
+    <li><a class='menu-links' href='#'  onclick='productos()'>Productos</a></li>\
     <li><a class='menu-links' href='#'>Logotipos</a></li>\
     <li><a class='menu-links' href='#'>Webs</a></li>\
     <li><a class='menu-links' href='#'>Colaboraciones</a></li>\
@@ -31,3 +31,48 @@ function portfolio(){
     </div>";
     document.querySelector('#content-h1').innerHTML = "<h1 class='name_portfolio'>Portfolio</h1>";
   };
+
+
+// popup
+
+function productos(){
+
+  var imagenes = [1,2,3,4,5,6,7,8,9];
+
+  document.querySelector('.content-block').innerHTML = "<div id='productos'>";
+  for(var i = 0; i <= 8; i++){
+  document.querySelector('.content-block').innerHTML += "<img onclick='ampliarProductos()' src='img/productos/"+ imagenes[i] +"_min.png'/>";
+  }
+  document.querySelector('.content-block').innerHTML += "</div>";
+
+}
+
+
+function ampliarProductos(){
+
+  var descripciones = ["descripcion 1","descripcion 2","descripcion 3","descripcion 4","descripcion 5","descripcion 6","descripcion 7","descripcion 8","descripcion 9"]
+  var imgProductos = [1,2,3,4,5,6,7,8,9]
+
+
+  document.querySelector('.content-block').innerHTML += "<div id='ampliar-foto'>";
+  for(var i = 0; i <= 8; i++){
+  document.querySelector('.content-block').innerHTML += "<p id='texto' class='pop-text'>"+ descripciones[i] +" <a class='cerrar btn' onclick='cerrar()'>Cerrar</a></p>"
+  for(var j = 0; j <= 8; j++){
+  document.querySelector('.content-block').innerHTML += "<img class='pop-image' src='img/productos/"+ imgProductos[j] +".png' />";
+  }
+  }
+
+  document.querySelector('.content-block').innerHTML += "</div>";
+
+
+
+}
+
+function cerrar(){
+  //document.querySelector("#ampliar-foto").classList.add('quit');
+  //setTimeout(function(){
+  //  document.querySelector("#ampliar-foto").remove();
+  //},500);
+  document.querySelector("#ampliar-foto").remove();
+
+}
