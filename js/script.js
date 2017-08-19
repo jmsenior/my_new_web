2a9218230bd97136f886ce6be3e530e4436e6776
José Manuel Serna Rodríguez <jm.sernarodriguez@gmail.com>
2017-08-19 17:32:15 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,8 +21,8 @@ function portfolio(){
     document.querySelector('#block-menu').innerHTML = "<div id='portfolio'>\
     <ul class='menu-list'>\
     <li><a class='menu-links' href='#'  onclick='productos()'>Productos</a></li>\
-    <li><a class='menu-links' href='#'>Logotipos</a></li>\
-    <li><a class='menu-links' href='#'>Webs</a></li>\
+    <li><a class='menu-links' href='#' onclick='logotipos()'>Logotipos</a></li>\
+    <li><a class='menu-links' href='#' onclick='webs()'>Webs</a></li>\
     <li><a class='menu-links' href='#'>Colaboraciones</a></li>\
     <li><a class='menu-links' href='#'>Carteles</a></li>\
     <li><a class='menu-links' href='#'>Videos</a></li>\
@@ -33,44 +33,90 @@ function portfolio(){
   };
 
 
-// popup
+  // productos
 
-function productos(){
+  function productos(){
 
-  var imagenes = [1,2,3,4,5,6,7,8,9];
-  var descripciones = ["descripcion1","descripcion2","descripcion3","descripcion4","descripcion5","descripcion6","descripcion7","descripcion8","descripcion9",]
+    var imagenes = [1,2,3,4,5,6,7,8,9];
+    var descripciones = ["descripcion1","descripcion2","descripcion3","descripcion4","descripcion5","descripcion6","descripcion7","descripcion8","descripcion9"]
 
-  document.querySelector('#content-h1').innerHTML = "<h1 class='name_portfolio'>Productos</h1>";
+    document.querySelector('#content-h1').innerHTML = "<h1 class='name_portfolio'>Productos</h1>";
 
-  document.querySelector('.content-block').innerHTML = "<div id='productos'>";
-  for(var i = 0; i <= 8; i++){
-    document.querySelector('#productos').innerHTML += "<div class='item'>\
-  <img onclick='ampliarProductos()' src='img/productos/" + imagenes[i] + "min.png'/>\
-  <div id='back-img' class='hide'>\
-  <p id='texto'> <a class='cerrar btn' onclick='cerrar()'>Cerrar</a></p>\
-  <img id='img-grande' src='img/productos/" + imagenes[i] + ".png' />\
-  </div>\
-  </div>";
+    document.querySelector('.content-block').innerHTML = "<div id='productos'>";
+    for(var i = 0; i <= 8; i++){
+      document.querySelector('#productos').innerHTML += "<div class='item'>\
+    <img onclick='ampliarProductos("+ imagenes[i] +")' src='img/productos/" + imagenes[i] + "_min.png'/>\
+    <div id='back-img" + imagenes[i] + "' class='hide'>\
+    <p id='texto" + imagenes[i] + "'>" + descripciones[i] + " <a class='cerrar btn' onclick='cerrar(" + imagenes[i] + ")'>Cerrar</a></p>\
+    <img id='img-grande" + imagenes[i] + "' src='img/productos/" + imagenes[i] + ".png' />\
+    </div>\
+    </div>";
+    }
+
+  //  document.querySelector('.content-block').innerHTML += "<a href='#' class='' onclick='portfolio()'>  Volver</a>";
+    document.querySelector('.content-block').innerHTML += "</div>";
+  }
+  // logotipos
+
+  function logotipos(){
+
+    var imagenes = [1,2,3,4,5,6,7,8,9];
+    var descripciones = ["descripcion1","descripcion2","descripcion3","descripcion4","descripcion5","descripcion6","descripcion7","descripcion8","descripcion9"]
+
+    document.querySelector('#content-h1').innerHTML = "<h1 class='name_portfolio'>Logotipos</h1>";
+
+    document.querySelector('.content-block').innerHTML = "<div id='logotipos'>";
+    for(var i = 0; i <= 8; i++){
+      document.querySelector('#logotipos').innerHTML += "<div class='item'>\
+    <img onclick='ampliarProductos("+ imagenes[i] +")' src='img/logotipos/" + imagenes[i] + "_min.png'/>\
+    <div id='back-img" + imagenes[i] + "' class='hide'>\
+    <p id='texto" + imagenes[i] + "'>" + descripciones[i] + " <a class='cerrar btn' onclick='cerrar(" + imagenes[i] + ")'>Cerrar</a></p>\
+    <img id='img-grande" + imagenes[i] + "' src='img/logotipos/" + imagenes[i] + ".png' />\
+    </div>\
+    </div>";
+    }
+
+  //  document.querySelector('.content-block').innerHTML += "<a href='#' class='' onclick='portfolio()'>  Volver</a>";
+    document.querySelector('.content-block').innerHTML += "</div>";
+  }
+  // webs
+
+  function webs(){
+
+    var imagenes = [1,2,3,4,5,6,7,8,9];
+    var descripciones = ["descripcion1","descripcion2","descripcion3","descripcion4","descripcion5","descripcion6","descripcion7","descripcion8","descripcion9"]
+
+    document.querySelector('#content-h1').innerHTML = "<h1 class='name_portfolio'>Webs</h1>";
+
+    document.querySelector('.content-block').innerHTML = "<div id='webs'>";
+    for(var i = 0; i <= 8; i++){
+      document.querySelector('#webs').innerHTML += "<div class='item'>\
+    <img onclick='ampliarProductos("+ imagenes[i] +")' src='img/webs/" + imagenes[i] + "_min.png'/>\
+    <div id='back-img" + imagenes[i] + "' class='hide'>\
+    <p id='texto" + imagenes[i] + "'>" + descripciones[i] + " <a class='cerrar btn' onclick='cerrar(" + imagenes[i] + ")'>Cerrar</a></p>\
+    <img id='img-grande" + imagenes[i] + "' src='img/webs/" + imagenes[i] + ".png' />\
+    </div>\
+    </div>";
+    }
+
+  //  document.querySelector('.content-block').innerHTML += "<a href='#' class='' onclick='portfolio()'>  Volver</a>";
+    document.querySelector('.content-block').innerHTML += "</div>";
   }
 
-  //document.querySelector('#productos').innerHTML += "<a href='#' class='' onclick='portfolio()'>  Volver</a>";
-  document.querySelector('.content-block').innerHTML += "</div>";
+//ampliar foto
 
-
-
+function ampliarProductos(index){
+	//console.log(index);
+ document.querySelector('#back-img' + index).classList.remove('hide');
+ document.querySelector('#back-img' + index).classList.add('ampliar-foto');
+  document.querySelector('#texto' + index).classList.add('pop-text');
+  document.querySelector('#img-grande' + index).classList.add('pop-image');
 }
 
-function ampliarProductos(){
- document.querySelector('#back-img').classList.remove('hide');
- document.querySelector('#back-img').classList.add('ampliar-foto');
-  document.querySelector('#texto').classList.add('pop-text');
-  document.querySelector('#img-grande').classList.add('pop-image');
-}
+// cerrar foto
 
-
-
-function cerrar(){
-  var objetoCerrar = document.querySelector('#back-img');
+function cerrar(index){
+  var objetoCerrar = document.querySelector('#back-img' + index);
 
 if ( objetoCerrar.classList.contains('hide')){
   objetoCerrar.classList.remove('hide');
